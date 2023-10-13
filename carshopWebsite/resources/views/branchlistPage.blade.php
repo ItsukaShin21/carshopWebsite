bddd7b2d793cfa5b58ab9b1fa1a288ded0aedcca
ItsukaShin21 <jef.lotoc@gmail.com>
2023-10-12 17:54:49 -0800

    Front-End Changes
    Branch Page Changes:
    
    -Added table for the data
    -Added button for Add, Edit, Delete
    -Navigation anchors
    -Temporary background design(will change if someone wants it)

--- carshopWebsite/resources/views/branchlistPage.blade.php
+++ carshopWebsite/resources/views/branchlistPage.blade.php
@@ -8,8 +8,10 @@
     <meta charset = "UTF-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
     <title>KotCheck | Branches</title>
+    <link rel = "stylesheet" href = "{{asset('css\styles.css')}}">
+    <link rel = "stylesheet" href = "{{asset('script\script.js')}}">
 </head>
 <body>
-    
+    <div class = "headerContainer1"></div>
 </body>
 </html>
